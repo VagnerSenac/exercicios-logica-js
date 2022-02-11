21cc860870247a65c2772abb674bdc3b62785518
vagnerinfo <53969916+vagnerinfo@users.noreply.github.com>
2022-02-10 22:23:22 -0300

    ex 17

---
+++ 017/script.js
@@ -0,0 +1,14 @@
+//Faça um Programa que peça dois números e imprima o maior deles.
+
+let num1, num2
+console.log("Digite um número")
+num1 = 20
+console.log("Digite outro número")
+num2 = 20
+if(num1 > num2) {
+    console.log("O primeiro número é o maior")
+}
+else{
+    console.log("O segundo número é o maior")
+}
+
